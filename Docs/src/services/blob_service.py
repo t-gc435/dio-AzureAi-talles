37a04f2575f69799438c5098a4e8d0354937c269
t-gc435 <168124405+t-gc435@users.noreply.github.com>
2024-11-29 19:01:45 -0300

    Add files via upload

---
+++ Docs/src/services/blob_service.py
@@ -0,0 +1,19 @@
+import os
+import streamlit as st 
+from utils.Config import Config
+from azure.storage.blob import BlobServiceClient
+
+def upload_blob(file, file_name):
+  try:
+    blob_service_client = BlobServiceClient.from_connection_string(Config.AZURE_STORAGE_CONNECTION_STRING)
+        
+    blob_client = blob_service_client.get_blob_client(container=Config.CONTAINER_NAME, blob=file_name)
+   
+    file_data = file.getvalue()
+         
+    blob_client.upload_blob(file_data, overwrite=True)
+        
+    return blob_client.url
+  except Exception as ex:
+    st.error(f"Erro ao enviar o arquivo para o Azure Blob Storage: {ex}")
+    return None
